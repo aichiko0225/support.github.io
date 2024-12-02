849f0384f6c31105e9c2e84b7bad899522a6804d
ash66 <aichiko66@163.com>
2024-12-02 09:46:55 +0800

    build generate production

--- src/Support.js
+++ src/Support.js
@@ -8,7 +8,16 @@ function SupportPage() {
       <p>This is the support page for your app.</p>
       <p>You can provide support to your users here.</p>
       <p>Thank you for using our app!</p>
-      
+
+      {/* contact us with email aichiko66@gmail.com, or call us with phone number */}
+      <p>
+        <a href="mailto:aichiko66@gmail.com">Contact Us</a>
+      </p>
+      <p>
+        <a href="tel:+1234567890">Call Us</a>
+      </p>
     </div>
   );
 }
+
+export default SupportPage;
