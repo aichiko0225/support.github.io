a6503160c56ac32031383ec2e66ea11d9d44ae9e
ash66 <aichiko66@163.com>
2024-12-02 09:31:44 +0800

    create react project

---
+++ src/Support.js
@@ -0,0 +1,14 @@
+// support site for app store review
+import React from 'react';
+
+function SupportPage() {
+  return (
+    <div>
+      <h1>Support Page</h1>
+      <p>This is the support page for your app.</p>
+      <p>You can provide support to your users here.</p>
+      <p>Thank you for using our app!</p>
+      
+    </div>
+  );
+}
